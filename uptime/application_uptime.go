4f2a8337ddc4631f570c6952b96b815d5dc5f621
marhjoh <marhjoh@stud.ntnu.no>
2023-03-05 02:35:03 +0100

    added documentation

--- uptime/application_uptime.go
+++ uptime/application_uptime.go
@@ -1,15 +1,17 @@
-//With inspiration from https://go.dev/play/p/by_nkvhzqD
-
 package uptime
 
+// This file contains provides the functionality for tracking the uptime of a service.
+// Was made with inspiration from https://go.dev/play/p/by_nkvhzqD
 import "time"
 
 var startTime time.Time
 
+// Init initializes the uptime tracking by setting the startTime variable to the current time.
 func Init() {
 	startTime = time.Now()
 }
 
+// GetUptime returns the uptime of the service in seconds.
 func GetUptime() int {
 	return int(time.Since(startTime).Seconds())
 }
